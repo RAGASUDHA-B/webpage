b045f2ab28533eeb15cda1318419ca4ca4df882f
RAGASUDHA B <ragasudha@student.tce.edu>
2025-03-30 00:22:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const apikey = '0a8189f475a8434ba7fa44c0748e1768'
+const blogcontainer = document.getElementById("blog-container");
+
+function fetchRandomevents(){
+    try{
+         const apiurl =
+    }catch(error){
+        console.error("error fetching random news",error)
+        return[]
+    }
+}
